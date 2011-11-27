2e6a3a4e759b189ecff3a94794daa29c273865fd
János Illés <ijanos@gmail.com>
2011-11-27 16:08:12 +0100

    Add a small utility script

---
+++ unused.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# List files with exactly one hard link number
+#
+# I use the files in this directory hard linked to their specific 
+# locations, so I can tell which files are currently in use by the 
+# number of hardlinks. If it is one, the file is not in use.
+
+find . -path "./.git/*" -prune -o -links 1 -print
