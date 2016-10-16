75a6817be436e33b0223a7facaf8c460d521d8d6
János Illés <ijanos@gmail.com>
2016-10-16 11:40:35 +0200

    [fish] set up user PATH properly without overwriting previous entries

--- fish/.config/fish/setup.fish
+++ fish/.config/fish/setup.fish
@@ -1,7 +1,8 @@
 # This script creates fish universal variables
 # Run the script once and the variables will persist
 
-set -U fish_user_paths ~/.local/bin/
+set -U fish_user_paths ~/.local/bin/ $fish_user_paths
+set -U fish_user_paths ~/.cargo/bin/ $fish_user_paths
 set -Ux MANWIDTH 80
 set -Ux EDITOR vim
 set -Ux LESS -Ri
