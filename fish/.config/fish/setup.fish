7f4a47e7a8dfe345f01be927f28a29fef10d941a
János Illés <ijanos@gmail.com>
2016-10-16 11:50:39 +0200

    [fish] erase the user_paths first

--- fish/.config/fish/setup.fish
+++ fish/.config/fish/setup.fish
@@ -1,6 +1,9 @@
 # This script creates fish universal variables
 # Run the script once and the variables will persist
 
+# erase the user_paths to avoid duplicates
+set -e fish_user_paths
+
 set -U fish_user_paths ~/.local/bin/ $fish_user_paths
 set -U fish_user_paths ~/.cargo/bin/ $fish_user_paths
 set -Ux MANWIDTH 80
