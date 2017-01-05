d60ff423a405a3af23266ed25092c9280d4e421b
János Illés <ijanos@gmail.com>
2017-01-05 19:07:09 +0100

    [fish] add npm bin directory to path

--- fish/.config/fish/setup.fish
+++ fish/.config/fish/setup.fish
@@ -6,6 +6,7 @@ set -e fish_user_paths
 
 set -U fish_user_paths ~/.local/bin/ $fish_user_paths
 set -U fish_user_paths ~/.cargo/bin/ $fish_user_paths
+set -U fish_user_paths ~/.npm-pkgs/bin $fish_user_paths
 set -Ux MANWIDTH 80
 set -Ux EDITOR vim
 set -Ux LESS -Ri
