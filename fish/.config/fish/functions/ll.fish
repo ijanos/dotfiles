3a3d50fe29550bda56d4e039216294371ea65a6b
János Illés <ijanos@gmail.com>
2023-04-08 10:36:04 +0200

    fish: use exa for listing file with ll

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,3 +1,4 @@
 function ll --description "List contents of directory using long format"
-	ls -lhXN --time-style=long-iso --group-directories-first $argv
+	#ls -lhXN --time-style=long-iso --group-directories-first $argv
+	exa --long --group-directories-first $argv
 end
