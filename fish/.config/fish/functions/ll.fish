131d4ec7eb005f66460cfd86b7101f49e73e77ed
János Illés <ijanos@gmail.com>
2024-12-28 10:18:04 +0100

    fish: switch back ll alias to ls
    exa is unmaintained, there are other forks but ls is good enough

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,4 +1,3 @@
 function ll --description "List contents of directory using long format"
-	#ls -lhXN --time-style=long-iso --group-directories-first $argv
-	exa --long --group-directories-first $argv
+	ls -lhXN --time-style=long-iso --group-directories-first $argv
 end
